61dbbb43cf4dbc5f1376569d3e29342ea5ee06d5
xuwuhui <wuhui.xu@newtouch.cn>
2019-12-20 10:04:40 +0800

    字符串匹配算法

---
+++ data_structure/src/main/java/com/example/StringMatch.java
@@ -0,0 +1,136 @@
+package com.example;
+
+/**
+ * <p><b>Description:</b>  摘录于数据结构算法之美
+ * <p><b>Company:</b>
+ *
+ * @author created by Jesse Hsu at 15:58 on 2019/12/16
+ * @version V0.1
+ * @classNmae StringMatch
+ */
+public class StringMatch {
+
+
+    private static final int SIZE = 256; // ASCII 全局变量或成员变量
+
+    /**
+     * @param matcher   模式串
+     * @param strLen    模式串的长度
+     * @param hashTable 散列表
+     */
+    private void generateBC(char[] matcher, int strLen, int[] hashTable) {
+        for (int i = 0; i < SIZE; ++i) {
+            hashTable[i] = -1; // 初始化hashTable
+        }
+        for (int i = 0; i < strLen; ++i) {
+            int ascii = (int) matcher[i]; // 计算matcher[i]的ASCII值
+            hashTable[ascii] = i;        //记录每个字符在模式串中的最后一个位置
+        }
+    }
+
+
+
+
+    // b表示模式串，m表示长度，suffix，prefix数组事先申请好了
+    private static void generateGS(char[] b, int m, int[] suffix, boolean[] prefix) {
+        for (int i = 0; i < m; ++i) { // 初始化
+            suffix[i] = -1;
+            prefix[i] = false;
+        }
+        for (int i = 0; i < m - 1; ++i) { // b[0, i]
+            int j = i;
+            int k = 0; // 公共后缀子串长度
+            while (j >= 0 && b[j] == b[m-1-k]) { // 与b[0, m-1]求公共后缀子串
+                --j;
+                ++k;
+                suffix[k] = j+1; //j+1表示公共后缀子串在b[0, i]中的起始下标
+            }
+            if (j == -1) prefix[k] = true; //如果公共后缀子串也是模式串的前缀子串
+        }
+    }
+
+
+    // a,b表示主串和模式串；n，m表示主串和模式串的长度。
+    public int bm(char[] a, int n, char[] b, int m) {
+        int[] bc = new int[SIZE]; // 记录模式串中每个字符最后出现的位置
+        generateBC(b, m, bc); // 构建坏字符哈希表
+        int[] suffix = new int[m];
+        boolean[] prefix = new boolean[m];
+        generateGS(b, m, suffix, prefix);
+        int i = 0; // j表示主串与模式串匹配的第一个字符
+        while (i <= n - m) {
+            int j;
+            for (j = m - 1; j >= 0; --j) { // 模式串从后往前匹配
+                if (a[i+j] != b[j]) break; // 坏字符对应模式串中的下标是j
+            }
+            if (j < 0) {
+                return i; // 匹配成功，返回主串与模式串第一个匹配的字符的位置
+            }
+            int x = j - bc[(int)a[i+j]];
+            int y = 0;
+            if (j < m-1) { // 如果有好后缀的话
+                y = moveByGS(j, m, suffix, prefix);
+            }
+            i = i + Math.max(x, y);
+        }
+        return -1;
+    }
+
+    // j表示坏字符对应的模式串中的字符下标; m表示模式串长度
+    private int moveByGS(int j, int m, int[] suffix, boolean[] prefix) {
+        int k = m - 1 - j; // 好后缀长度
+        if (suffix[k] != -1) return j - suffix[k] +1;
+        for (int r = j+2; r <= m-1; ++r) {
+            if (prefix[m-r] == true) {
+                return r;
+            }
+        }
+        return m;
+    }
+
+
+    // a, b分别是主串和模式串；n, m分别是主串和模式串的长度。
+    public static int kmp(char[] a, int n, char[] b, int m) {
+        int[] next = getNexts(b, m);
+        int j = 0;
+        for (int i = 0; i < n; ++i) {
+            while (j > 0 && a[i] != b[j]) { // 一直找到a[i]和b[j]
+                j = next[j - 1] + 1;
+            }
+            //j可能小于等于0
+            if (a[i] == b[j]) {
+                ++j;
+            }
+            if (j == m) { // 找到匹配模式串的了
+                return i - m + 1;
+            }
+        }
+        return -1;
+    }
+
+
+    // b表示模式串，m表示模式串的长度
+    private static int[] getNexts(char[] b, int m) {
+        int[] next = new int[m];
+        next[0] = -1;
+        int k = -1;
+        for (int i = 1; i < m; ++i) {
+            while (k != -1 && b[k + 1] != b[i]) {
+                k = next[k];
+            }
+            if (b[k + 1] == b[i]) {
+                ++k;
+            }
+            next[i] = k;
+        }
+        return next;
+    }
+    public static void main(String[] args) {
+        char[] b={'c','a','b','c','a','b'};
+        int[] suffix = new int[b.length];
+        boolean[] prefix = new boolean[b.length];
+        generateGS(b,b.length,suffix,prefix);
+        System.out.println(suffix);
+        System.out.println(prefix);
+    }
+}
