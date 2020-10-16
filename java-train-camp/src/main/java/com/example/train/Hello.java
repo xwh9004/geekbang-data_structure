9a93f2c2876b5c0b328f4d3f22a4799166df7ef3
xuwuhui <wuhui.xu@newtouch.cn>
2020-10-16 16:24:38 +0800

    极客时间 java训练营demo

---
+++ java-train-camp/src/main/java/com/example/train/Hello.java
@@ -0,0 +1,60 @@
+package com.example.train;
+
+/**
+ * <p><b>Description:</b>
+ * TODO
+ * <p><b>Company:</b>
+ *
+ * @author created by Jesse Hsu at 14:31 on 2020/10/16
+ * @version V0.1
+ * @classNmae Hello
+ */
+public class Hello {
+    public static void main(String[] args) {
+        Hello h = new Hello();
+        int m =2;
+        int n =3;
+        double a =1;
+        double b =6;
+        double c =5;
+        if(c<b){
+            h.add(a,b);
+        }else{
+            h.sub(a,b);
+        }
+        int time =10;
+        b =1.0;a=1.0;
+        for (int i=0;i<time;i++) {
+           h.multi(a,b);
+        }
+
+
+    }
+
+    public double add(double a,double b){
+        Hello h = this;
+        double num1 =a;
+        double num2 =b;
+        double num3 = num1+num2;
+        return num3;
+    }
+
+    public double sub(double a,double b){
+        double num1 =a;
+        double num2 =b;
+        double num3 = num1-num2;
+        return num3;
+    }
+    public double multi(double a,double b){
+        double num1 =a;
+        double num2 =b;
+        double num3 = num1*num2;
+        return num3;
+    }
+    public double divide(double a,double b){
+        double num1 =a;
+        double num2 =b;
+        double num3 = num1/num2;
+        return num3;
+    }
+}
