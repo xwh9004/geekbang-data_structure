b0092ad59197350ae9fbbe35b4f25ad8ffc88a16
xuwuhui <wuhui.xu@newtouch.cn>
2020-10-26 17:56:51 +0800

    java训练营 HttpClient 网关初代

--- java-train-camp/src/main/java/com/example/train/week2/MyGateWay.java
+++ java-train-camp/src/main/java/com/example/train/week2/MyGateWay.java
@@ -1,9 +1,17 @@
 package com.example.train.week2;
 
-import sun.net.www.http.HttpClient;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClients;
 
 import java.io.IOException;
-import java.net.URL;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * <p><b>Description:</b>
@@ -17,11 +25,20 @@ import java.net.URL;
 public class MyGateWay {
 
     public static void main(String[] args) {
-        try {
-            HttpClient client = HttpClient.New(new URL("http://localhost:8801"));
-//            client.
 
-        } catch (IOException e) {
+        HttpClient client = HttpClients.createDefault();
+        HttpUriRequest uri = new HttpGet();
+      
+        try {
+            ((HttpGet) uri).setURI(new URI("http://localhost:8801/"));
+            HttpResponse response = client.execute(uri);
+            HttpEntity responseEntity = response.getEntity();
+            InputStream content = responseEntity.getContent();
+            byte[] buf=new byte[1024];
+            int len = content.read(buf);
+            String rest = new String(buf,0,len);
+            System.out.println(rest);
+        } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
         }
     }
