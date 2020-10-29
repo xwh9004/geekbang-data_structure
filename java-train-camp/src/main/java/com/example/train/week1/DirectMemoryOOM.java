36432a53c7fde4a293247d40c2bbdac7ddb6dc3e
xuwuhui <wuhui.xu@newtouch.cn>
2020-10-29 10:49:39 +0800

    直接内存溢出演示

---
+++ java-train-camp/src/main/java/com/example/train/week1/DirectMemoryOOM.java
@@ -0,0 +1,27 @@
+package com.example.train.week1;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+/**
+ * <p><b>Description:</b>
+ * <p><b>Company:</b>
+ * VM Args： -Xmx20M-XX:MaxDirectMemorySize=10M
+ * 没有产生heap dump文件
+ * @author created by Jesse Hsu at 14:06 on 2020/10/28
+ * @version V0.1
+ * @classNmae DirectMemoryOOM
+ */
+public class DirectMemoryOOM {
+
+    private static final int _1MB = 1024 * 1024;
+    public static void main(String[] args) throws Exception {
+        Field unsafeField = Unsafe.class.getDeclaredFields()[0];
+        unsafeField.setAccessible(true);
+        Unsafe unsafe = (Unsafe) unsafeField.get(null);
+        while (true) {
+            unsafe.allocateMemory(_1MB);
+        }
+    }
+}
---
+++ java-train-camp/src/main/java/com/example/train/week2/netty/server/NettyServerApplication.java
@@ -0,0 +1,14 @@
+package com.example.train.week2.netty.server;
+
+
+public class NettyServerApplication {
+
+    public static void main(String[] args) {
+        HttpServer server = new HttpServer(false,8808);
+        try {
+            server.run();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}
