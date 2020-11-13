fe7dd4134f1e8bce29c8b31d1e475a6cfdad4407
xuwuhui <wuhui.xu@newtouch.cn>
2020-11-13 15:41:35 +0800

    创建netty 服务器接收文件

--- java-train-camp/src/main/java/com/example/train/io/server/fileServer/FileServerInboundHandler.java
+++ java-train-camp/src/main/java/com/example/train/io/server/fileServer/FileServerInboundHandler.java
@@ -1,11 +1,22 @@
 package com.example.train.io.server.fileServer;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelFutureListener;
-import io.netty.channel.ChannelHandler;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.*;
+import io.netty.handler.codec.http.*;
+import io.netty.handler.codec.http.cookie.Cookie;
+import io.netty.handler.codec.http.cookie.ServerCookieDecoder;
+import io.netty.handler.codec.http.cookie.ServerCookieEncoder;
+import io.netty.handler.codec.http.multipart.*;
+import io.netty.util.CharsetUtil;
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Collections;
+import java.util.Set;
+
+
 
 /**
  * <p><b>Description:</b>
@@ -19,6 +30,26 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @ChannelHandler.Sharable
 public class FileServerInboundHandler extends ChannelInboundHandlerAdapter {
+
+
+    private HttpPostRequestDecoder decoder;
+
+    private  HttpRequest request;
+
+    private HttpData partialContent;
+
+    private final StringBuilder responseContent = new StringBuilder();
+
+    private static final HttpDataFactory factory =
+            new DefaultHttpDataFactory(DefaultHttpDataFactory.MINSIZE); // Disk if size exceed
+
+    static {
+        DiskFileUpload.deleteOnExitTemporaryFile = true; // should delete file on exit (in normal  exit)
+        DiskFileUpload.baseDirectory = null; // system temp directory
+        DiskAttribute.deleteOnExitTemporaryFile = true; // should delete file on  exit (in normal exit)
+        DiskAttribute.baseDirectory = null; // system temp directory
+    }
+
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         super.channelActive(ctx);
@@ -34,8 +65,51 @@ public class FileServerInboundHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         log.info("FileServer channelRead! " );
-    }
+        if (msg instanceof HttpRequest) {
+            HttpRequest request = this.request = (HttpRequest) msg;
+            decoder = new HttpPostRequestDecoder(factory, request);
+            //是否分片
+            boolean readingChunks = HttpUtil.isTransferEncodingChunked(request);
 
+            responseContent.append("Is Chunked: " + readingChunks + "\r\n");
+            responseContent.append("IsMultipart: " + decoder.isMultipart() + "\r\n");
+        }
+
+        if (decoder != null) {
+            if (msg instanceof HttpContent) {
+                // New chunk is received
+                HttpContent chunk = (HttpContent) msg;
+                try {
+                    decoder.offer(chunk);
+                } catch (HttpPostRequestDecoder.ErrorDataDecoderException e1) {
+                    e1.printStackTrace();
+                    responseContent.append(e1.getMessage());
+                    writeResponse(ctx.channel(), true);
+                    return;
+                }
+
+                readHttpDataChunkByChunk();
+
+                // example of reading only if at the end
+                if (chunk instanceof LastHttpContent) {
+
+                    ByteBuf content = chunk.content();
+
+                    writeResponse(ctx.channel(),false);
+
+                    reset();
+                }
+            }
+        }
+
+    }
+    private void reset() {
+        request = null;
+
+        // destroy the decoder to release all resources
+        decoder.destroy();
+        decoder = null;
+    }
 
     @Override
     public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
@@ -68,5 +142,121 @@ public class FileServerInboundHandler extends ChannelInboundHandlerAdapter {
     public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
         super.channelUnregistered(ctx);
         log.info("FileServer channelUnregistered! " );
+    }
+
+    /**
+     * Example of reading request by chunk and getting values from chunk to chunk
+     */
+    private void readHttpDataChunkByChunk()throws Exception {
+        try {
+            //
+            while (decoder.hasNext()) {
+                InterfaceHttpData data = decoder.next();
+                if (data != null) {
+                    // check if current HttpData is a FileUpload and previously set as partial
+                    if (partialContent == data) {
+                        log.info(" 100% (FinalSize: " + partialContent.length() + ")");
+                        if (partialContent instanceof FileUpload) {
+
+                            FileUpload fileUpload = ((FileUpload) partialContent);
+                            String fileName = fileUpload.getFilename();
+
+                            File file =  new File(fileName);
+                            try( FileOutputStream fou = new FileOutputStream(file)){
+                                if(!file.exists()){
+                                    file.createNewFile();
+                                    log.info("create files");
+                                }
+                                log.info(file.getAbsolutePath());
+                                fou.write(fileUpload.get());
+
+                                fou.flush();
+                                log.info("thread id = "+Thread.currentThread().getId());
+                                log.info("FileServerInboundHandler = "+FileServerInboundHandler.this.toString());
+                                log.info("create write done!");
+                            }
+                        }
+                        partialContent = null;
+                    }
+                    // new value
+//                    writeHttpData(data);
+                }
+            }
+            // Check partial decoding for a FileUpload
+            InterfaceHttpData data = decoder.currentPartialHttpData();
+            if (data != null) {
+                StringBuilder builder = new StringBuilder();
+                if (partialContent == null) {
+                    partialContent = (HttpData) data;
+                    if (partialContent instanceof FileUpload) {
+                        builder.append("Start FileUpload: ")
+                                .append(((FileUpload) partialContent).getFilename()).append(" ");
+                    } else {
+                        builder.append("Start Attribute: ")
+                                .append(partialContent.getName()).append(" ");
+                    }
+                    builder.append("(DefinedSize: ").append(partialContent.definedLength()).append(")");
+                }
+                if (partialContent.definedLength() > 0) {
+                    builder.append(" ").append(partialContent.length() * 100 / partialContent.definedLength())
+                            .append("% ");
+                    log.info(builder.toString());
+
+
+
+                } else {
+                    builder.append(" ").append(partialContent.length()).append(" ");
+                    log.info(builder.toString());
+                }
+            }else{
+                log.info(" InterfaceHttpData data =null");
+            }
+
+        } catch (HttpPostRequestDecoder.EndOfDataDecoderException e1) {
+
+            responseContent.append("\r\n\r\nEND OF CONTENT CHUNK BY CHUNK\r\n\r\n");
+        }
+    }
+
+
+    private void writeResponse(Channel channel, boolean forceClose) {
+        // Convert the response content to a ChannelBuffer.
+        ByteBuf buf = Unpooled.copiedBuffer(responseContent.toString(), CharsetUtil.UTF_8);
+        responseContent.setLength(0);
+
+        // Decide whether to close the connection or not.
+        boolean keepAlive = HttpUtil.isKeepAlive(request) && !forceClose;
+
+        // Build the response object.
+        FullHttpResponse response = new DefaultFullHttpResponse(
+                HttpVersion.HTTP_1_1, HttpResponseStatus.OK, buf);
+        response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain; charset=UTF-8");
+        response.headers().setInt(HttpHeaderNames.CONTENT_LENGTH, buf.readableBytes());
+
+        if (!keepAlive) {
+            response.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.CLOSE);
+        } else if (request.protocolVersion().equals(HttpVersion.HTTP_1_0)) {
+            response.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE);
+        }
+
+        Set<Cookie> cookies;
+        String value = request.headers().get(HttpHeaderNames.COOKIE);
+        if (value == null) {
+            cookies = Collections.emptySet();
+        } else {
+            cookies = ServerCookieDecoder.STRICT.decode(value);
+        }
+        if (!cookies.isEmpty()) {
+            // Reset the cookies if necessary.
+            for (Cookie cookie : cookies) {
+                response.headers().add(HttpHeaderNames.SET_COOKIE, ServerCookieEncoder.STRICT.encode(cookie));
+            }
+        }
+        // Write the response.
+        ChannelFuture future = channel.writeAndFlush(response);
+        // Close the connection after the write operation is done if necessary.
+        if (!keepAlive) {
+            future.addListener(ChannelFutureListener.CLOSE);
+        }
     }
 }
